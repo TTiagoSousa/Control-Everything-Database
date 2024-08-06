fde420fca9b824aeda992d2fac4b8a55b614f3d5
ttiagosousa <ttiagopintosousa97@gmail.com>
2024-08-06 10:58:00 +0100

    Create article fix

--- src/articles/articles.controller.ts
+++ src/articles/articles.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
 import { ArticlesService } from './articles.service';
+import { Controller, Post, UseGuards, Req, Body } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth-user/jwt.guard';
+import { Request } from 'express';
+import { createArticle_dto } from './dto/create.article_dto';
 
 @Controller('articles')
 export class ArticlesController {
   constructor(private readonly articlesService: ArticlesService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Post('create')
+  async createArticle(@Req() req: Request, @Body() dto: createArticle_dto) {
+    const userId = req.user['id'];
+    return this.articlesService.createArticle(dto, userId);
+  }
 }
--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { createArticle } from './helpers/create.article';
+import { createArticle_dto } from './dto/create.article_dto';
 
 @Injectable()
-export class ArticlesService {}
+export class ArticlesService {
+
+  async createArticle(dto: createArticle_dto, employeeId: string) {
+    const result = await createArticle(dto, employeeId);
+    return result;
+  }
+
+}
