966ad644e2abb1058a735586bd9023996ce2e1ab
ttiagosousa <ttiagopintosousa97@gmail.com>
2024-08-06 18:42:36 +0100

    Update token timer

--- src/utils/token/employee.signin.token.ts
+++ src/utils/token/employee.signin.token.ts
@@ -1,14 +1,13 @@
-import { EmployeeRole} from "@prisma/client";
-import { jwtSecret } from "../constants"
+import { EmployeeRole } from "@prisma/client";
+import { jwtSecret } from "../constants";
 import { JwtService } from '@nestjs/jwt';
-
 
 export async function employeeCreateToken(args: { id: string; email: string; EmployeeRole?: EmployeeRole }) {
   const payload = args;
 
   const jwtService = new JwtService();
-  const token = await jwtService.signAsync(payload, { secret: jwtSecret, expiresIn: '30m' });
-  const refreshToken = await jwtService.signAsync(payload, { secret: jwtSecret, expiresIn: '50m' });
+  const token = await jwtService.signAsync(payload, { secret: jwtSecret, expiresIn: '1d' });
+  const refreshToken = await jwtService.signAsync(payload, { secret: jwtSecret, expiresIn: '3d' });
   
   return { token, refreshToken };
 }
