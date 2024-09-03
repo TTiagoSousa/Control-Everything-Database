f2c06175088e79bbfc807628fe083d4a55a624e5
ttiagosousa <ttiagopintosousa97@gmail.com>
2024-09-03 08:18:41 +0100

    Sign in user

--- src/auth-user/auth-user.controller.ts
+++ src/auth-user/auth-user.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Req, Res } from '@nestjs/common';
 import { AuthUserService } from './auth-user.service';
 import { signup_dto } from './dto/sing.up.user.dto';
+import { signin_user_dto } from './dto/sign.in.user.dto';
 
 @Controller('auth-user')
 export class AuthUserController {
@@ -10,5 +11,11 @@ export class AuthUserController {
   async signup_User(@Body() dto: signup_dto) {
 
     return this.authUserService.signupUser(dto);
+  }
+
+  @Post('sign-in')
+  async signinUser(@Body() dto: signin_user_dto, @Req() req, @Res() res) {
+
+    return this.authUserService.signinUser(dto, req, res);
   }
 }
--- src/auth-user/auth-user.service.ts
+++ src/auth-user/auth-user.service.ts
@@ -3,6 +3,8 @@ import { JwtService } from '@nestjs/jwt';
 import { EmailService } from 'src/email/email.service';
 import { signup_dto } from './dto/sing.up.user.dto';
 import { signupUser } from './helpers/sign.up.user';
+import { signin_user_dto } from './dto/sign.in.user.dto';
+import { signinUser } from './helpers/sign.in.user';
 
 @Injectable()
 export class AuthUserService {
@@ -13,6 +15,11 @@ export class AuthUserService {
 
   async signupUser(dto: signup_dto) {
     const result = await signupUser(dto, this.jwt, this.emailService);
+    return result;
+  }
+
+  async signinUser(dto: signin_user_dto, req, res) {
+    const result = await signinUser(dto, this.jwt, req, res);
     return result;
   }
 }
