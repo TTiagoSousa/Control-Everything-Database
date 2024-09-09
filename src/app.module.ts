7bc1195c865683c5f7b6af7ec5cefde4c5cf5fd4
ttiagosousa <ttiagopintosousa97@gmail.com>
2024-09-09 03:30:45 +0100

    Auth employee service, controller and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UserModule } from './user/user.module';
 import { AuthUserModule } from './auth-user/auth-user.module';
 import { EmailModule } from './email/email.module';
 import { CurrenciesModule } from './currencies/currencies.module';
+import { AuthEmployeeModule } from './auth-employee/auth-employee.module';
 
 @Module({
-  imports: [UserModule, AuthUserModule, EmailModule, CurrenciesModule],
+  imports: [UserModule, AuthUserModule, EmailModule, CurrenciesModule, AuthEmployeeModule],
   controllers: [AppController],
   providers: [AppService],
 })
