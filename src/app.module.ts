e67950311aaf60100906648dfb5988f80767dcc0
ttiagosousa <ttiagopintosousa97@gmail.com>
2024-10-08 17:19:26 +0100

    Brokers module, service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,9 +9,10 @@ import { AuthEmployeeModule } from './auth-employee/auth-employee.module';
 import { EmployeeModule } from './employee/employee.module';
 import { PasswordResetModule } from './password-reset/password-reset.module';
 import { AutorizedBrokerApiModule } from './autorized-broker-api/autorized-broker-api.module';
+import { BrokersModule } from './brokers/brokers.module';
 
 @Module({
-  imports: [UserModule, AuthUserModule, EmailModule, CurrenciesModule, AuthEmployeeModule, EmployeeModule, PasswordResetModule, AutorizedBrokerApiModule],
+  imports: [UserModule, AuthUserModule, EmailModule, CurrenciesModule, AuthEmployeeModule, EmployeeModule, PasswordResetModule, AutorizedBrokerApiModule, BrokersModule],
   controllers: [AppController],
   providers: [AppService],
 })
