f605febca6d7192f3c3b6f5582978a8ac4940e01
ttiagosousa <ttiagopintosousa97@gmail.com>
2024-10-11 01:50:10 +0100

    Add new platform function

--- src/platforms/platforms.controller.ts
+++ src/platforms/platforms.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { PlatformsService } from './platforms.service';
+import { addNewPlatform_dto } from './dto/add.new.platform';
+import { Roles } from 'src/employee/decorators/roles.decorator';
+import { EmployeeRole } from '@prisma/client';
+import { JwtAuthGuard } from 'src/auth-user/jwt.guard';
+import { EmployeeRolesGuard } from 'src/employee/guards/roles.guard';
 
 @Controller('platforms')
 export class PlatformsController {
   constructor(private readonly platformsService: PlatformsService) {}
+
+  @UseGuards(JwtAuthGuard, EmployeeRolesGuard)
+  @Roles(EmployeeRole.ADMIN)
+  @Post('add-new-platform')
+  async signup_User(@Body() dto: addNewPlatform_dto) {
+
+    return this.platformsService.addNewPlatform(dto);
+  }
 }
--- src/platforms/platforms.service.ts
+++ src/platforms/platforms.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { addNewPlatform } from './helpers/add.new.platform';
+import { addNewPlatform_dto } from './dto/add.new.platform';
 
 @Injectable()
-export class PlatformsService {}
+export class PlatformsService {
+
+  async addNewPlatform(dto: addNewPlatform_dto) {
+    const result = await addNewPlatform(dto);
+    return result;
+  }
+
+}
