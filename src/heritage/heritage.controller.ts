408c242a4a7a4b3601ec7c6360a4b048f06c5eb7
ttiagosousa <ttiagopintosousa97@gmail.com>
2024-10-20 21:11:09 +0100

    Get total heritage

--- src/heritage/heritage.controller.ts
+++ src/heritage/heritage.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { HeritageService } from './heritage.service';
+import { JwtAuthGuard } from 'src/auth-user/jwt.guard';
 
 @Controller('heritage')
 export class HeritageController {
   constructor(private readonly heritageService: HeritageService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get(':userId/get-total-converted/:targetConvertion')
+  async getTotalOnSavingsTransitionsConverted(
+    @Param('userId') userId: string,
+    @Param('targetConvertion') targetConvertion: string,
+  ) {
+    return this.heritageService.getTotalHeritageConverted(userId, targetConvertion);
+  }
 }
