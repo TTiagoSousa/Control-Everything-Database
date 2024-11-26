516d01597d01f369ffb23edc7950fa223de47fd0
ttiagosousa <ttiagopintosousa97@gmail.com>
2024-11-26 13:39:01 +0000

    Email service, controller and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthUserModule } from './auth-user/auth-user.module';
 import { UserModule } from './user/user.module';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [AuthUserModule, UserModule],
+  imports: [AuthUserModule, UserModule, EmailModule],
   controllers: [AppController],
   providers: [AppService],
 })
