f56149c17eb014cb7e531b35c0c0c414080d35f3
ttiagosousa <ttiagopintosousa97@gmail.com>
2024-11-26 18:24:27 +0000

    Employee service, controller and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthUserModule } from './auth-user/auth-user.module';
 import { UserModule } from './user/user.module';
 import { EmailModule } from './email/email.module';
 import { AuthEmployeeModule } from './auth-employee/auth-employee.module';
+import { EmployeeModule } from './employee/employee.module';
 
 @Module({
-  imports: [AuthUserModule, UserModule, EmailModule, AuthEmployeeModule],
+  imports: [AuthUserModule, UserModule, EmailModule, AuthEmployeeModule, EmployeeModule],
   controllers: [AppController],
   providers: [AppService],
 })
