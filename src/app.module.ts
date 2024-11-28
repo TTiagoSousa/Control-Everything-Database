bcfe57e5ac285d407395a5ae7c42ea2d23356e03
ttiagosousa <ttiagopintosousa97@gmail.com>
2024-11-28 00:30:24 +0000

    Crypto transitions module, service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,10 @@ import { EmailModule } from './email/email.module';
 import { AuthEmployeeModule } from './auth-employee/auth-employee.module';
 import { EmployeeModule } from './employee/employee.module';
 import { CurrenciesModule } from './currencies/currencies.module';
+import { CryptoTransitionsModule } from './crypto-transitions/crypto-transitions.module';
 
 @Module({
-  imports: [AuthUserModule, UserModule, EmailModule, AuthEmployeeModule, EmployeeModule, CurrenciesModule],
+  imports: [AuthUserModule, UserModule, EmailModule, AuthEmployeeModule, EmployeeModule, CurrenciesModule, CryptoTransitionsModule],
   controllers: [AppController],
   providers: [AppService],
 })
